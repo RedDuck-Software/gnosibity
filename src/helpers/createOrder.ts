4ea39069fb95246c363235a7df1fa748be665cb5
tymurpolishchuk <tymurpolishchukwork@gmail.com>
2022-09-12 19:02:30 +0300

    Added posibility to create orders (theoritically)

--- src/helpers/createOrder.ts
+++ src/helpers/createOrder.ts
@@ -1,15 +1,19 @@
 import { Order, Owner } from "@bity/api/models";
 
-export function createOrder(cryptoAddress: string | undefined): Order {
-  if (typeof cryptoAddress === 'undefined')
-    return new Order()
-  const ibanNum = 'CH0400766000103138557';
+type Currency = 'ETH' | 'BTC';
+
+export function createOrder(cryptoAddress: string | undefined, sendingCurrency : Currency, 
+  gettingCurrency : Currency,
+  valueToSent : number, valueToGet : number): Promise<Order> {
+  if (typeof cryptoAddress === 'undefined'){
+    const order = new Order();
+    return Promise.resolve(order);
+  }
   const order = new Order();
   return order
-    .setInput('CHF', '50.00')
-    .do((input) => input.setOwner(new Owner()).setIban(ibanNum))
-    .setOutput('ETH', '50.00')
+    .setInput(sendingCurrency, valueToSent.toString())
+    .setOutput(gettingCurrency, valueToGet.toString())
     .do((output) =>
       output.setCryptoAddress(cryptoAddress)
-    );
+    ).generateObjectForOrderCreation();
 }
