abc2747ace16845505a5620908be8ebd71d3177d
Ilya Kubariev <ilya.kubariev@redduck.io>
2022-09-13 14:09:07 +0300

    feat: added getting of output value

--- src/pages/ExchangePage/ExchangePage.tsx
+++ src/pages/ExchangePage/ExchangePage.tsx
@@ -1,3 +1,4 @@
+import { CurrencyAndAmount } from '@bity/api/models';
 import { OrderPaymentDetails } from '@bity/api/models/order-payment-details';
 import React, { FC, useEffect, useState } from 'react';
 import { AiOutlineDown } from 'react-icons/ai';
@@ -12,39 +13,82 @@ import { createOrder } from '../../helpers/createOrder';
 import { useBityApi } from '../../hooks/useBityApi';
 import { useWallet } from '../../hooks/useWallet';
 
+type Metadata = {
+  value: string | undefined;
+  txCost: CurrencyAndAmount | undefined;
+  fee: CurrencyAndAmount | undefined;
+};
+
 const ExchangePage: FC = () => {
+  const SENDING_CURRENCY = 'ETH';
+  const GETTING_CURRENCY = 'USDT';
+
   const navigate = useNavigate();
   const { active, account, activate, provider } = useWallet();
   const { bityApi } = useBityApi();
   const [toAddress, setToAddress] = useState<string>('');
-  const [fromValue, setFromValue] = useState<number>(0);
+  const [fromValue, setFromValue] = useState<number>(1);
   const [toValue, setToValue] = useState<number>(0);
-  const [price, setPrice] = useState<string>(''); 
+  const [price, setPrice] = useState<number>(0);
+  const [txCost, setTxCost] = useState<CurrencyAndAmount>({
+    amount: '0',
+    currency: GETTING_CURRENCY,
+  });
+  const [fee, setFee] = useState<CurrencyAndAmount>({
+    amount: '0',
+    currency: SENDING_CURRENCY,
+  });
   const handleConnectWallet = () => activate();
-  useEffect(function () {
-    const fetchData = async () => {
-      const preparedOrder = await createOrder(
-        'account',
-        'toAddress',
-        'ETH',
-        'USDT',
-        1,
-        1,
-      );
-      const exchangeRate = await bityApi.fetchOrderExchangeRate(preparedOrder);
-      return exchangeRate;
+
+  const fetchMetadata = async (amountIn: number): Promise<Metadata> => {
+    const preparedOrder = await createOrder(
+      'account',
+      'toAddress',
+      SENDING_CURRENCY,
+      GETTING_CURRENCY,
+      amountIn,
+      1,
+    );
+    const exchangeRate = await bityApi.fetchOrderExchangeRate(preparedOrder);
+    return {
+      value: exchangeRate.output.amount.value,
+      txCost: exchangeRate.priceBreakdown?.outputTransactionCost,
+      fee: exchangeRate.priceBreakdown?.customerTradingFee,
+    };
+  };
+
+  const displayMetadata = (metadata: Metadata) => {
+    const price = parseFloat(metadata.value || '0');
+    setPrice(price);
+    setToValue(price);
+    if (metadata.txCost) {
+      setTxCost(metadata.txCost);
     }
-    fetchData().then(b => {
-      setPrice(b.output.amount.value ?? '');
-    })
-  }, [])
-  
+    if (metadata.fee) {
+      setFee(metadata.fee);
+    }
+  };
+
+  useEffect(() => {
+    fetchMetadata(1).then((metadata) => {
+      displayMetadata(metadata);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (fromValue) {
+      fetchMetadata(fromValue).then((metadata) => {
+        displayMetadata(metadata);
+      });
+    }
+  }, [fromValue]);
+
   const handleExchange = async () => {
     const preparedOrder = await createOrder(
       account,
       toAddress,
-      'ETH',
-      'USDT',
+      SENDING_CURRENCY,
+      GETTING_CURRENCY,
       fromValue,
       toValue,
     );
@@ -90,6 +134,7 @@ const ExchangePage: FC = () => {
               className={style.transferPropInput}
               placeholder="Your address"
               defaultValue={account}
+              disabled
             />
           ) : (
             <button onClick={handleConnectWallet}>Connect wallet</button>
@@ -100,6 +145,7 @@ const ExchangePage: FC = () => {
             type="number"
             className={style.transferPropInput}
             placeholder="You SEND"
+            value={fromValue}
             onChange={(e) => setFromValue(+e.target.value)}
           />
           <div className={style.currencySelector}>
@@ -107,7 +153,9 @@ const ExchangePage: FC = () => {
               <div className={style.currencySelectorIcon}>
                 <img src={EthLogo} alt="" />
               </div>
-              <div className={style.currencySelectorTicker}>ETH</div>
+              <div className={style.currencySelectorTicker}>
+                {SENDING_CURRENCY}
+              </div>
             </div>
           </div>
         </div>
@@ -116,7 +164,8 @@ const ExchangePage: FC = () => {
             type="number"
             className={style.transferPropInput}
             placeholder="You GET"
-            onChange={(e) => setToValue(+e.target.value)}
+            value={toValue}
+            disabled
           />
           <div className={style.currencySelector}>
             <div className={style.currencySelectorContent}>
@@ -124,7 +173,9 @@ const ExchangePage: FC = () => {
                 <img src={UsdtLogo} alt="" />
                 <AiOutlineDown className={style.currencySelectorArrow} />
               </div>
-              <div className={style.currencySelectorTicker}>USDT</div>
+              <div className={style.currencySelectorTicker}>
+                {GETTING_CURRENCY}
+              </div>
             </div>
           </div>
         </div>
@@ -137,11 +188,13 @@ const ExchangePage: FC = () => {
           />
         </div>
         <ul className={style.ulStyles}>
-          <li className={style.liStyles}>Trading fee: 1.60 CFH</li>
           <li className={style.liStyles}>
-            Cryptocurrency transaction cost 0.000025 BTC
+            Trading fee: {fee.amount} {fee.currency}
           </li>
-          <li className={style.liStyles}>Exchange rate {price ?? 0}</li>
+          <li className={style.liStyles}>
+            Cryptocurrency transaction cost {txCost.amount} {txCost.currency}
+          </li>
+          <li className={style.liStyles}>Exchange rate 1 / {price ?? 0}</li>
         </ul>
 
         <div onClick={handleExchange} className={style.confirmButton}>
